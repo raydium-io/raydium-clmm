2b38689b6292a7b8bda369cae6f0cc208658c0e3
0x777A <eddy@raydium.io>
2022-10-31 15:02:38 +0800

    fix collect remaining rewards

--- programs/amm/src/instructions/admin/collect_remaining_rewards.rs
+++ programs/amm/src/instructions/admin/collect_remaining_rewards.rs
@@ -25,8 +25,32 @@ pub fn collect_remaining_rewards(
     ctx: Context<CollectRemainingRewards>,
     reward_index: u8,
 ) -> Result<()> {
+    let amount_remaining = get_remaining_reward_amount(
+        &ctx.accounts.pool_state,
+        &ctx.accounts.reward_token_vault,
+        &ctx.accounts.reward_funder.key(),
+        reward_index,
+    )?;
+
+    transfer_from_pool_vault_to_user(
+        &ctx.accounts.pool_state,
+        &ctx.accounts.reward_token_vault,
+        &ctx.accounts.funder_token_account,
+        &ctx.accounts.token_program,
+        amount_remaining,
+    )?;
+
+    Ok(())
+}
+
+fn get_remaining_reward_amount(
+    pool_state_loader: &AccountLoader<PoolState>,
+    reward_token_vault: &Account<TokenAccount>,
+    reward_funder: &Pubkey,
+    reward_index: u8,
+) -> Result<u64> {
     let current_timestamp = u64::try_from(Clock::get()?.unix_timestamp).unwrap();
-    let mut pool_state = ctx.accounts.pool_state.load_mut()?;
+    let mut pool_state = pool_state_loader.load_mut()?;
     pool_state.update_reward_infos(current_timestamp)?;
 
     let reward_info = pool_state.reward_infos[reward_index as usize];
@@ -40,28 +64,16 @@ pub fn collect_remaining_rewards(
     );
     // require_keys_eq!(ctx.accounts.reward_funder.key(), reward_info.authority);
     require!(
-        ctx.accounts.reward_funder.key() == crate::admin::id(),
+        reward_funder.key() == crate::admin::id(),
         ErrorCode::NotApproved
     );
-    require_keys_eq!(
-        ctx.accounts.reward_token_vault.key(),
-        reward_info.token_vault
-    );
 
-    let amount_remaining = ctx
-        .accounts
-        .reward_token_vault
+    require_keys_eq!(reward_token_vault.key(), reward_info.token_vault);
+    
+    let amount_remaining = reward_token_vault
         .amount
         .checked_sub(reward_info.reward_total_emissioned)
         .unwrap();
 
-    transfer_from_pool_vault_to_user(
-        &ctx.accounts.pool_state,
-        &ctx.accounts.reward_token_vault,
-        &ctx.accounts.funder_token_account,
-        &ctx.accounts.token_program,
-        amount_remaining,
-    )?;
-
-    Ok(())
+    Ok(amount_remaining)
 }
