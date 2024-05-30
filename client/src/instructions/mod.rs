2eb458dfd634bb28521e25a81da3e93fa8926644
RainRaydium <106001368+RainRaydium@users.noreply.github.com>
2024-05-31 01:12:41 +0800

    Fix: add security_txt and MINT_WHITELIST (#66)

--- client/src/instructions/mod.rs
+++ client/src/instructions/mod.rs
@@ -1,10 +1,5 @@
-pub mod token_instructions;
-pub use token_instructions::*;
 pub mod amm_instructions;
-pub use amm_instructions::*;
-pub mod rpc;
-pub use rpc::*;
-pub mod utils;
-pub use utils::*;
 pub mod events_instructions_parse;
-pub use events_instructions_parse::*;
+pub mod rpc;
+pub mod token_instructions;
+pub mod utils;
